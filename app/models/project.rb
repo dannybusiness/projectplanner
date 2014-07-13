3ef35827549b9b63c13e27bcf7f17c4e4ecde3d2
Daniel Mortensen <daniel.j.mortensen@gmail.com>
2014-07-13 11:06:25 +0100

    show validation errors in view when creating new project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,13 @@ class Project < ActiveRecord::Base
 	belongs_to :user
 
 	validates :name, :planning_start_date, :planning_end_date, :implementation_start_date, :implementation_end_date, :presence => true
+	#validate :ordered_dates?
+
+	def ordered_dates?
+		if planning_start_date.present? && planning_start_date > planning_end_date
+			errors.add(:date_order, 'Planning Start Date is occurs after Planning End Date')
+		end
+	end
+
+
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -8,8 +8,19 @@ describe Project do
 	it 'is invalid without a name' do 
 		expect(FactoryGirl.build(:project, name: nil)).to_not be_valid
 	end
-	it 'is invalid without a planning start date'
-	it 'is invalid without a planning end date'
-	it 'is invalid without an implementation start date'
-	it 'is invalid without an implementation end date'
+	it 'is invalid without a planning start date' do 
+		expect(FactoryGirl.build(:project, planning_start_date: nil)).to_not be_valid
+	end
+	it 'is invalid without a planning end date' do
+		expect(FactoryGirl.build(:project, planning_end_date: nil)).to_not be_valid
+	end
+	it 'is invalid without an implementation start date' do
+		expect(FactoryGirl.build(:project, implementation_start_date: nil)).to_not be_valid
+	end
+	it 'is invalid without an implementation end date' do
+		expect(FactoryGirl.build(:project, implementation_end_date: nil)).to_not be_valid
+	end
+	it 'is invalid with improperly ordered start and end dates ' do
+		expect(FactoryGirl.build(:project, planning_start_date: '31/12/2014')).to_not be_valid
+	end
 end
